f56f6b53663ea0686c8b21a02056f3c55ba3a603
John Leavell <johnleavell@gmail.com>
2023-10-24 10:55:43 -0500

    added product model

---
+++ db/migrate/20231024155413_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :stripe_product_id
+      t.string :stripe_price_id
+      t.string :currency, default: "usd"
+      t.integer "sales_count", default: 0, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_002659) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_24_155413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
+
+  create_table "checkouts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "stripe_product_id"
+    t.string "stripe_price_id"
+    t.string "currency", default: "usd"
+    t.integer "sales_count", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.citext "email", default: "", null: false
