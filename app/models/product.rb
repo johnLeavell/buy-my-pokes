221fb4d87674e685bd6652165364ecdcde45b898
John Leavell <johnleavell@gmail.com>
2023-10-24 15:26:50 -0500

    added products model and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+    validates :name, presence: true
+    validates :price, numericality: { greater_than: 0, less_than: 1000000 }
+
+    def to_s
+        name
+    end
+    
+    monetize :price, as: :price_cents
+
+    after_create do 
+        product = Stripe::Product.create(name: name)    
+        price = Stripe::Price.create(product: product, unit_amount: self.price, currency: self.currency)  
+        update(stripe_product_id: product.id, stripe_price_id: price.id)
+    end
+
 end
--- db/migrate/20231024155413_create_products.rb
+++ db/migrate/20231024155413_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration[7.1]
       t.string :stripe_product_id
       t.string :stripe_price_id
       t.string :currency, default: "usd"
-      t.integer "sales_count", default: 0, null: false
+      t.integer :sales_count, default: 0, null: false
 
       t.timestamps
     end
