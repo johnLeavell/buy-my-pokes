025a9a21a97d26bf79dda1806ceaa790dcdac6e2
John Leavell <johnleavell@gmail.com>
2023-10-26 19:38:30 -0500

    added create stripe user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :citext           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :citext
+#  last_name              :citext
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :citext
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  stripe_customer_id     :string
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -8,8 +31,19 @@ class User < ApplicationRecord
     "#{first_name} #{last_name}"
   end        
   
-  def to_s
-    email
-  end
+  def stripe_customer
+    return create_strip_customer unless stripe_customer_id.present?
 
+      Stripe::Customer.retrieve(stripe_customer_id)
+    rescue Stripe::InvalidRequestError
+      create_strip_customer
+    end
+
+    private
+
+    def create_strip_customer
+      customer = Stripe::Customer.create(email: self.email)
+      update(stripe_customer_id: customer.id)
+      customer
+    end
 end
